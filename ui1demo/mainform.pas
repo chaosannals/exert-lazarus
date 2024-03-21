f761824fb0f77542545b7b72cf06072b3e64f2ee
chaosannals <chaosannals@outlook.com>
2024-03-21 17:37:46 +0800

    save

---
+++ ui1demo/mainform.pas
@@ -0,0 +1,26 @@
+unit mainform;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TMainForm = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
